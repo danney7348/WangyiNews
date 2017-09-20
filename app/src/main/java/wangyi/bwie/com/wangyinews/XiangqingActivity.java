df76af56d373356d421b46b0b43aa34a2400c7ff
danney7348 <31384817+danney7348@users.noreply.github.com>
2017-09-20 15:10:16 +0800

    新闻详情页面根据手势滑动来关闭该页面

--- app/src/main/java/wangyi/bwie/com/wangyinews/XiangqingActivity.java
+++ app/src/main/java/wangyi/bwie/com/wangyinews/XiangqingActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
 
-public class XiangqingActivity extends AppCompatActivity {
+import me.imid.swipebacklayout.lib.SwipeBackLayout;
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
+public class XiangqingActivity extends SwipeBackActivity {
 
     private String url;
 
@@ -14,6 +17,9 @@ public class XiangqingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xiangqing);
+        SwipeBackLayout swipeBackLayout = getSwipeBackLayout();
+        //设置滑动方向，可设置EDGE_LEFT, EDGE_RIGHT, EDGE_ALL, EDGE_BOTTOM
+        swipeBackLayout.setEdgeTrackingEnabled(SwipeBackLayout.EDGE_LEFT);
         WebView wv = (WebView) findViewById(R.id.wv);
         Intent intent = getIntent();
         url = intent.getStringExtra("url");
