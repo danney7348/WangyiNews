18c63002575d29c28f2cadef7876bf69d8f48b9e
danney7348 <31384817+danney7348@users.noreply.github.com>
2017-09-20 14:54:19 +0800

    QQ分享

--- app/src/main/java/wangyi/bwie/com/wangyinews/XiangqingActivity.java
+++ app/src/main/java/wangyi/bwie/com/wangyinews/XiangqingActivity.java
@@ -8,13 +8,15 @@ import android.webkit.WebView;
 
 public class XiangqingActivity extends AppCompatActivity {
 
+    private String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_xiangqing);
         WebView wv = (WebView) findViewById(R.id.wv);
         Intent intent = getIntent();
-        String url = intent.getStringExtra("url");
+        url = intent.getStringExtra("url");
         String title = intent.getStringExtra("title");
         wv.getSettings().setJavaScriptEnabled(true);
         wv.loadUrl(url);
@@ -22,7 +24,7 @@ public class XiangqingActivity extends AppCompatActivity {
     }
     public void dianji(View view){
         Intent intent1=new Intent(Intent.ACTION_SEND);
-        intent1.putExtra(Intent.EXTRA_TEXT,"This is my text to send");
+        intent1.putExtra(Intent.EXTRA_TEXT,url);
         intent1.setType("text/plain");
         startActivity(Intent.createChooser(intent1,"share"));
     }
